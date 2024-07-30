a74ab7695bb2f8dea00e8f00068ca18bba46f2b6
Enzo Zamberlan <enzo2017.bra@gmail.com>
2024-07-29 21:59:48 -0300

    Testando a criação de tarefas

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,3 +1,5 @@
+const tbody = document.querySelector('tbody');
+
 const fetchTasks = async () => {
   const response = await fetch('http://localhost:3333/tasks');
   const tasks = await response.json();
@@ -12,12 +14,23 @@ const createElement = (tag, innerText = '') => {
   return element
 }
 
+const task = {
+  id: 1,
+  title: 'Teste',
+  created_at: '00 de Janeiro 2024 00:00',
+  status: 'pendente'
+}
+
 const createRow = (task) => {
 
   const { id, title, created_at, status } = task;
 
   const tr = createElement('tr');
-  const tdTitle = createElement('td', 'titulo da task');
+  const tdTitle = createElement('td', title);
+
+  tr.appendChild(tdTitle);
+
+  tBody.appendChild(tr);
 }
 
-createRow({});
+createRow(task);
